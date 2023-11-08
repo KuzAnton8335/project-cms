d7ca3670bf32b17f5b2fcf1f4f4a79527432642b
KuzAnton8335 <ant2783@gmail.com>
2023-11-08 15:07:29 +0300

    add scripts/main.js

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalInputDiscount = document.querySelector('.modal__input_discount');
