281a4cfb451dbbecf601f0b83a86e24e1b5922ee
KuzAnton8335 <ant2783@gmail.com>
2023-11-15 15:45:48 +0300

    delete row

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,16 @@ const overlay = document.querySelector('.overlay');
 const overlayModal = document.querySelector('.overlay__modal');
 const panelAddGoods = document.querySelector('.panel__add-goods');
 const modalClose = document.querySelector('.modal__close');
+const tableBody = document.querySelector('.table__body');
+const btnDel = document.querySelector('.table__btn_del');
+
 
 
 
 const createRow = (obj) => {
-  const row = document.createElement('tr');
-  row.innerHTML = `
+	const row = document.createElement('tr');
+	row.classList.add('table__row');
+	row.innerHTML = `
 	<td class="table__cell">${obj.id}</td>
       <td
       	class="table__cell table__cell_left table__cell_name"
@@ -32,93 +36,101 @@ const createRow = (obj) => {
                     <button class="table__btn table__btn_del"></button>
                   </td>
 	`
-  return row;
+	return row;
 }
 
 
 const renderGoods = (goods) => {
-  const table = document.querySelector('.table__body');
-  goods.forEach((obj) => {
-    const row = createRow(obj);
-    table.appendChild(row);
-  });
+	const table = document.querySelector('.table__body');
+	goods.forEach((obj) => {
+		const row = createRow(obj);
+		table.appendChild(row);
+	});
 }
 
 
 
 const goods = [
-  {
-    "id": 3,
-    "title": "Смартфон Xiaomi 11T 8/128GB",
-    "price": 27000,
-    "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
-    "category": "mobile-phone",
-    "discont": false,
-    "count": 3,
-    "units": "шт",
-    "images": {
-      "small": "img/smrtxiaomi11t-m.jpg",
-      "big": "img/smrtxiaomi11t-b.jpg"
-    }
-  },
-  {
-    "id": 4,
-    "title": "Радиоуправляемый автомобиль Cheetan",
-    "price": 4000,
-    "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
-    "category": "toys",
-    "discont": 5,
-    "count": 1,
-    "units": "шт",
-    "images": {
-      "small": "img/cheetancar-m.jpg",
-      "big": "img/cheetancar-b.jpg"
-    }
-  },
-  {
-    "id": 5,
-    "title": "ТВ приставка MECOOL KI",
-    "price": 12400,
-    "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
-    "category": "tv-box",
-    "discont": 15,
-    "count": 4,
-    "units": "шт",
-    "images": {
-      "small": "img/tvboxmecool-m.jpg",
-      "big": "img/tvboxmecool-b.jpg"
-    }
-  },
-  {
-    "id": 6,
-    "title": "Витая пара PROConnect 01-0043-3-25",
-    "price": 22,
-    "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
-    "category": "cables",
-    "discont": false,
-    "count": 420,
-    "units": "v",
-    "images": {
-      "small": "img/lan_proconnect43-3-25.jpg",
-      "big": "img/lan_proconnect43-3-25-b.jpg"
-    }
-  }
+	{
+		"id": 3,
+		"title": "Смартфон Xiaomi 11T 8/128GB",
+		"price": 27000,
+		"description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
+		"category": "mobile-phone",
+		"discont": false,
+		"count": 3,
+		"units": "шт",
+		"images": {
+			"small": "img/smrtxiaomi11t-m.jpg",
+			"big": "img/smrtxiaomi11t-b.jpg"
+		}
+	},
+	{
+		"id": 4,
+		"title": "Радиоуправляемый автомобиль Cheetan",
+		"price": 4000,
+		"description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
+		"category": "toys",
+		"discont": 5,
+		"count": 1,
+		"units": "шт",
+		"images": {
+			"small": "img/cheetancar-m.jpg",
+			"big": "img/cheetancar-b.jpg"
+		}
+	},
+	{
+		"id": 5,
+		"title": "ТВ приставка MECOOL KI",
+		"price": 12400,
+		"description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
+		"category": "tv-box",
+		"discont": 15,
+		"count": 4,
+		"units": "шт",
+		"images": {
+			"small": "img/tvboxmecool-m.jpg",
+			"big": "img/tvboxmecool-b.jpg"
+		}
+	},
+	{
+		"id": 6,
+		"title": "Витая пара PROConnect 01-0043-3-25",
+		"price": 22,
+		"description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
+		"category": "cables",
+		"discont": false,
+		"count": 420,
+		"units": "v",
+		"images": {
+			"small": "img/lan_proconnect43-3-25.jpg",
+			"big": "img/lan_proconnect43-3-25-b.jpg"
+		}
+	}
 ];
 
 renderGoods(goods);
 
 panelAddGoods.addEventListener('click', () => {
-  overlay.classList.add('active');
+	overlay.classList.add('active');
 });
 
-overlayModal.addEventListener('click', event => {
-  event.stopPropagation();
-})
-
-modalClose.addEventListener('click', () => {
-  overlay.classList.remove('active');
+overlay.addEventListener('click', (e) => {
+	const target = e.target;
+	if (target === overlay || target.closest('.modal__close')) {
+		overlay.classList.remove('active');
+	}
 });
 
-overlay.addEventListener('click', () => {
-  overlay.classList.remove('active');
+tableBody.addEventListener('click', (e) => {
+	const target = e.target;
+	if (target.closest('.table__btn_del')) {
+		target.closest('.table__row').remove();
+	}
+	const row = document.querySelector('.table__row');
+	const id = row.obj.id
+	if (target.closest('.table__btn_del')) {
+		target.closest(id).remove();
+		console.log(obj);
+	}
 })
